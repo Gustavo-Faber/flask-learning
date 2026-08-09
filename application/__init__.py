3765019db42a265efa11b06744d5c4dcd656fcd6
Gustavo Sérgio <263528374+Gustavo-Faber@users.noreply.github.com>
2026-08-09 17:51:57 -0300

    db

--- application/__init__.py
+++ application/__init__.py
@@ -10,3 +10,4 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from application import routes
+from application import models
--- application/models.py
+++ application/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class Contatos(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    shipping_date = db.Column(db.DateTime, default=datetime.utcnow())
+    shipping_date = db.Column(db.DateTime, default = datetime.utcnow())
     name = db.Column(db.String, nullable = True)
     email = db.Column(db.String, nullable = True)
     subject = db.Column(db.String, nullable = True)
