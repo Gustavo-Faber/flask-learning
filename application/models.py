f60a6b3b65e2cd3ea26da221fdcd42f26df0c14b
Gustavo Sérgio <263528374+Gustavo-Faber@users.noreply.github.com>
2026-08-09 17:32:17 -0300

    database

---
+++ application/models.py
@@ -0,0 +1,10 @@
+from application import db
+from datetime import datetime
+
+class Contatos(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    shipping_date = db.Column(db.DateTime, default=datetime.utcnow())
+    name = db.Column(db.String, nullable = True)
+    email = db.Column(db.String, nullable = True)
+    subject = db.Column(db.String, nullable = True)
+    message = db.Column(db.String, nullable = True)
